4bcbe9ab5bf01645fda3ced6f55b1a6b50812e9e
Mark Linton <redshirt.linton@gmail.com>
2013-09-23 22:11:03 -0700

    Initial command line additions

--- MediaBrowser.Theater.Core/MediaPlayers/DefaultCommandArgs.cs
+++ MediaBrowser.Theater.Core/MediaPlayers/DefaultCommandArgs.cs
@@ -13,10 +13,45 @@ namespace MediaBrowser.Theater.Core.MediaPlayers
                 return string.Empty;
             }
 
+            // MEDIA PLAYERS
             if (string.Equals(filename, "mpc-hc.exe"))
             {
                 return "{PATH} /play /close /fullscreen";
             }
+
+            // GAMES
+            if (string.Equals(filename, "mupen64.exe"))
+            {
+                return "-g {PATH} -nogui";
+            }
+
+            if (string.Equals(filename, "1964.exe"))
+            {
+                return "-f -g {PATH}";
+            }
+
+            if (string.Equals(filename, "dolphin.exe"))
+            {
+                return "--exec={PATH}";
+            }
+
+            if (string.Equals(filename, "fusion.exe"))
+            {
+                return "-fullscreen {PATH}";
+            }
+
+            if (string.Equals(filename, "epsxe.exe"))
+            {
+                return "-nogui -loadbin {PATH}";
+            }
+
+            // They add the release version to the filename ie: pcsx2-r5350.exe
+            if (filename.ToLowerInvariant().Contains("pcsx2"))
+            {
+                return "--nogui --fullscreen {PATH}";
+            }
+
+            // BOOKS
 
             return string.Empty;
         }

563c6bb0854e9929a5dda4b47f04973a8d02e1fb
Andrew Van Til <andyvt@babgvant.com>
2014-01-16 20:16:09 +0000

    Support LAV audio/video configuration
    - Default to HWA for video
    - Default to PCM output for audio
    - Define LAV filter interfaces

--- MediaBrowser.Theater.Interfaces/Configuration/InternalPlayerConfiguration.cs
+++ MediaBrowser.Theater.Interfaces/Configuration/InternalPlayerConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Management;
 namespace MediaBrowser.Theater.Interfaces.Configuration
 {
     /// <summary>
@@ -23,5 +24,85 @@ namespace MediaBrowser.Theater.Interfaces.Configuration
         /// </summary>
         /// <value><c>true</c> if [enable xy sub filter]; otherwise, <c>false</c>.</value>
         public bool EnableXySubFilter { get; set; }
+
+        public VideoConfiguration VideoConfig { get; set; }
+
+        public AudioConfiguration AudioConfig { get; set; }
+
+        public InternalPlayerConfiguration()
+        {
+            //set defaults if necessary
+            VideoConfig = new VideoConfiguration();
+            AudioConfig = new AudioConfiguration();
+        }
+    }
+
+    //add configuration values here as necessary
+    public class VideoConfiguration
+    {
+        //we only need to do this once per run
+        private static string _gpuModel = string.Empty;
+        public static string GpuModel
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_gpuModel))
+                {
+                    //this may not work for multi-GPU systems
+                    using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DisplayConfiguration"))
+                    {
+                        foreach (ManagementObject mo in searcher.Get())
+                        {
+                            foreach (PropertyData property in mo.Properties)
+                            {
+                                if (property.Name == "Description")
+                                {
+                                    _gpuModel = property.Value.ToString();
+                                    break;
+                                }
+                            }
+                            mo.Dispose();
+                        }
+                    }
+                }
+                return _gpuModel;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating overridden video HWA mode.
+        /// </summary>
+        /// <value><c>> -1</c> if user has overridden; otherwise, <c>-1</c>.</value>
+        private int _hwaMode = -1;
+        public int HwaMode
+        {
+            get
+            {
+                if (_hwaMode > -1)
+                    return _hwaMode;
+                else
+                {
+                    if (GpuModel.IndexOf("Intel") > -1)
+                        return 2; //LAVHWAccel.QuickSync;
+                    else
+                        return 3; // LAVHWAccel.DXVA2CopyBack;
+                }
+            }
+            set
+            {
+                _hwaMode = value;
+            }
+        }
+    }
+
+    //add configuration values here as necessary
+    public class AudioConfiguration
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether [enable audio bitstreaming].
+        /// </summary>
+        /// <value><c>true</c> if [enable audio bitstreaming]; otherwise, <c>false</c>.</value>
+        public bool EnableAudioBitstreaming { get; set; }
+
     }
 }

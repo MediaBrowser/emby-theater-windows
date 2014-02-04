b2b882767ebaa21a4170af12cdf3706fcb3138af
Thomas Gillen <thomas.gillen@googlemail.com>
2014-02-04 22:31:35 +0000

    Added base view model

---
+++ MediaBrowser.Theater/MediaBrowser.Theater.Api/Theming/ViewModels/BaseViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using MediaBrowser.Theater.Api.Annotations;
+
+namespace MediaBrowser.Theater.Api.Theming.ViewModels
+{
+    /// <summary>
+    ///     The base class for view models.
+    /// </summary>
+    public abstract class BaseViewModel
+        : INotifyPropertyChanged
+    {
+        private static readonly Task Completed = Task.FromResult<object>(null);
+
+        private bool _isActive;
+        private bool _isInitialized;
+
+        /// <summary>
+        ///     Gets or sets a value indicating if this view model is active.
+        /// </summary>
+        public bool IsActive
+        {
+            get { return _isActive; }
+            set
+            {
+                if (value.Equals(_isActive)) return;
+                _isActive = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        ///     Gets a value indicating if this view model has been initialized.
+        /// </summary>
+        public bool IsInitialized
+        {
+            get { return _isInitialized; }
+            private set
+            {
+                if (value.Equals(_isInitialized)) return;
+                _isInitialized = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        ///     Notifies listeners that a property has changed.
+        /// </summary>
+        /// <param name="propertyName">The name of the property which has changed.</param>
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            Action action = () =>
+            {
+                PropertyChangedEventHandler handler = PropertyChanged;
+                if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+            };
+
+            action.OnUiThread();
+        }
+
+        /// <summary>
+        ///     Initializes this view model.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        public virtual Task Initialize()
+        {
+            IsInitialized = true;
+            return Completed;
+        }
+    }
+}
---
+++ MediaBrowser.Theater/MediaBrowser.Theater.Api/Theming/ViewModels/UIDispatchExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace MediaBrowser.Theater.Api.Theming.ViewModels
+{
+    public static class UIDispatchExtensions
+    {
+        private static readonly Task Completed = Task.FromResult<object>(null);
+        private static Dispatcher _dispatcher;
+
+        private static Dispatcher Dispatcher
+        {
+            get { return _dispatcher ?? (_dispatcher = Dispatcher.CurrentDispatcher); }
+        }
+
+        /// <summary>
+        ///     Executes the specified action on the UI thread.
+        /// </summary>
+        /// <param name="action">The action to execute.</param>
+        public static void OnUiThread(this Action action)
+        {
+            if (Dispatcher.CheckAccess())
+                action();
+            else
+                Dispatcher.Invoke(action);
+        }
+
+        /// <summary>
+        ///     Asynchronously executes the specified action on the UI thread.
+        /// </summary>
+        /// <param name="action">The action to execute.</param>
+        /// <returns>A task representing the asychronous operation.</returns>
+        public static Task OnUiThreadAsync(this Action action)
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                action();
+                return Completed;
+            }
+
+            var completionSource = new TaskCompletionSource<object>();
+
+            Action wrapper = () =>
+            {
+                try
+                {
+                    action();
+                    completionSource.SetResult(null);
+                }
+                catch (Exception e)
+                {
+                    completionSource.SetException(e);
+                }
+            };
+
+            Dispatcher.BeginInvoke(wrapper);
+            return completionSource.Task;
+        }
+    }
+}

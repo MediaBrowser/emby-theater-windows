d9194fb44a7734b5116dd31e2c778187fea77805
Thomas Gillen <thomas.gillen@googlemail.com>
2014-02-04 23:13:41 +0000

    Added navigation paths

---
+++ MediaBrowser.Theater/MediaBrowser.Theater.Api/Theming/Navigation/DefaultPaths.cs
@@ -0,0 +1,81 @@
+﻿using MediaBrowser.Model.Dto;
+
+namespace MediaBrowser.Theater.Api.Theming.Navigation
+{
+    // Application wide paths that all themes are expected to implement
+
+    /// <summary>
+    ///     The path to the home page.
+    /// </summary>
+    public class HomePath : NavigationPath { }
+
+    /// <summary>
+    ///     The path to application settings.
+    /// </summary>
+    public class SettingsPath : NavigationPath { }
+
+    /// <summary>
+    ///     The path to the currently playing media page.
+    /// </summary>
+    public class ActiveMediaPath : NavigationPath { }
+
+    /// <summary>
+    ///     The path to the "Now Playing" playlist.
+    /// </summary>
+    public class NowPlayingPlaylistPath : NavigationPath { }
+
+    /// <summary>
+    ///     The path to the page for the specified item.
+    /// </summary>
+    public class ItemPath : NavigationPathArg<BaseItemDto> { }
+
+    #region Fluent Interface
+
+    /// <summary>
+    ///     Contains extension methods to the Go class, to add an API for creating the default navigation paths.
+    /// </summary>
+    public static class DefaultPathExtensions
+    {
+        /// <summary>
+        ///     Gets a path to the home path.
+        /// </summary>
+        /// <param name="go"></param>
+        /// <returns>A path to the home page.</returns>
+        public static HomePath Home(this Go go)
+        {
+            return new HomePath();
+        }
+
+        /// <summary>
+        ///     Gets a path to the settings page.
+        /// </summary>
+        /// <param name="go"></param>
+        /// <returns>A path to the settings page.</returns>
+        public static SettingsPath Settings(this Go go)
+        {
+            return new SettingsPath();
+        }
+
+        /// <summary>
+        ///     Gets a path to the active media page.
+        /// </summary>
+        /// <param name="go"></param>
+        /// <returns>A path to the active media page.</returns>
+        public static ActiveMediaPath ActiveMedia(this Go go)
+        {
+            return new ActiveMediaPath();
+        }
+
+        /// <summary>
+        ///     Gets a path to the specified items page.
+        /// </summary>
+        /// <param name="item">The item to view.</param>
+        /// <returns>A path to the page for the specified item.</returns>
+        public static ItemPath Item(BaseItemDto item)
+        {
+            return new ItemPath { Parameter = item };
+        }
+    }
+
+    #endregion
+}
---
+++ MediaBrowser.Theater/MediaBrowser.Theater.Api/Theming/Navigation/INavigationService.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+
+namespace MediaBrowser.Theater.Api.Theming.Navigation
+{
+    /// <summary>
+    ///     The INavigationService interface defines the API for instructing a theme to
+    ///     display the content relevant to a specified path.
+    /// </summary>
+    public interface INavigationService
+    {
+        /// <summary>
+        ///     Gets the current location.
+        /// </summary>
+        INavigationPath CurrentLocation { get; }
+
+        /// <summary>
+        ///     Gets a value indicating if the theme can currently navigate back.
+        /// </summary>
+        bool CanGoBack { get; }
+
+        /// <summary>
+        ///     Gets a value indicating if the them can currently navigate forward.
+        /// </summary>
+        bool CanGoForward { get; }
+
+        /// <summary>
+        ///     Instructs the theme to display the content relevant to the given path.
+        /// </summary>
+        /// <param name="path">The path to navigate to.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task Navigate(INavigationPath path);
+
+        /// <summary>
+        ///     Navigates back to the previous location.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task Back();
+
+        /// <summary>
+        ///     Navigates forward to the next location.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task Forward();
+
+        /// <summary>
+        ///     Clears the navigation history.
+        /// </summary>
+        /// <param name="count">The number of items to remove from the history.</param>
+        void ClearNavigationHistory(int count = int.MaxValue);
+    }
+}

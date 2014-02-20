8298695ef973be54f4f063f346f18a6bf24bdcfe
Thomas Gillen <thomas.gillen@googlemail.com>
2014-02-20 22:23:20 +0000

    Added transition effects to ExtendedContentControl

--- MediaBrowser.Theater/MediaBrowser.Theater.Presentation/Controls/ExtendedContentControl.cs
+++ MediaBrowser.Theater/MediaBrowser.Theater.Presentation/Controls/ExtendedContentControl.cs
@@ -1,10 +1,16 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 using MediaBrowser.Theater.Api.UserInterface;
 using MediaBrowser.Theater.Presentation.ViewModels;
+using Microsoft.Expression.Media.Effects;
 
 namespace MediaBrowser.Theater.Presentation.Controls
 {
+    /// <summary>
+    /// http://victorcher.blogspot.com/2012/02/wpf-transactions.html
+    /// </summary>
     [TemplatePart(Name = "PART_ContentPresenter", Type = typeof (ContentPresenter))]
     public class ExtendedContentControl
         : ContentControl
@@ -13,6 +19,15 @@ namespace MediaBrowser.Theater.Presentation.Controls
         ///     The _content presenter
         /// </summary>
         private ContentPresenter _contentPresenter;
+
+        /// <summary>
+        ///     Initializes static members of the <see cref="ExtendedContentControl" /> class.
+        /// </summary>
+        static ExtendedContentControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof (ExtendedContentControl), new FrameworkPropertyMetadata(typeof (ExtendedContentControl)));
+            ContentProperty.OverrideMetadata(typeof (ExtendedContentControl), new FrameworkPropertyMetadata(OnContentPropertyChanged));
+        }
 
         private ContentPresenter ContentPresenter
         {
@@ -24,14 +39,48 @@ namespace MediaBrowser.Theater.Presentation.Controls
             }
         }
 
+        #region DP TransitionType
+
+        // Using a DependencyProperty as the backing store for TransitionType.  This enables animation, styling, binding, etc...
         /// <summary>
-        ///     Initializes static members of the <see cref="ExtendedContentControl" /> class.
+        ///     The transition type property
         /// </summary>
-        static ExtendedContentControl()
+        public static readonly DependencyProperty TransitionTypeProperty =
+            DependencyProperty.Register("TransitionType", typeof (TransitionEffect), typeof (ExtendedContentControl),
+                                        new UIPropertyMetadata(null));
+
+        /// <summary>
+        ///     Gets or sets the type of the transition.
+        /// </summary>
+        /// <value>The type of the transition.</value>
+        public TransitionEffect TransitionType
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof (ExtendedContentControl), new FrameworkPropertyMetadata(typeof (ExtendedContentControl)));
-            ContentProperty.OverrideMetadata(typeof (ExtendedContentControl), new FrameworkPropertyMetadata(OnContentPropertyChanged));
+            get { return (TransitionEffect) GetValue(TransitionTypeProperty); }
+            set { SetValue(TransitionTypeProperty, value); }
         }
+
+        #endregion DP TransitionType
+
+        #region DP Transition Animation
+
+        // Using a DependencyProperty as the backing store for TransitionAnimation.  This enables animation, styling, binding, etc...
+        /// <summary>
+        ///     The transition animation property
+        /// </summary>
+        public static readonly DependencyProperty TransitionAnimationProperty =
+            DependencyProperty.Register("TransitionAnimation", typeof (DoubleAnimation), typeof (ExtendedContentControl), new UIPropertyMetadata(null));
+
+        /// <summary>
+        ///     Gets or sets the transition animation.
+        /// </summary>
+        /// <value>The transition animation.</value>
+        public DoubleAnimation TransitionAnimation
+        {
+            get { return (DoubleAnimation) GetValue(TransitionAnimationProperty); }
+            set { SetValue(TransitionAnimationProperty, value); }
+        }
+
+        #endregion DP Transition Animation
 
         /// <summary>
         ///     When overridden in a derived class, is invoked whenever application code or internal processes call
@@ -78,8 +127,42 @@ namespace MediaBrowser.Theater.Presentation.Controls
                     activatable.IsActive = true;
                 }
 
-                ContentPresenter.Content = content;
+                ChangeDisplayedContent(content);
             }
+        }
+
+        private void ChangeDisplayedContent(object content)
+        {
+            FrameworkElement oldContentVisual;
+
+            try {
+                oldContentVisual = VisualTreeHelper.GetChild(_contentPresenter, 0) as FrameworkElement;
+            }
+            catch {
+                _contentPresenter.Content = content;
+                return;
+            }
+
+            TransitionEffect transitionEffect = TransitionType;
+            int renderingTier = (RenderCapability.Tier >> 16);
+
+            if (renderingTier < 2 || transitionEffect == null) {
+                _contentPresenter.Content = content;
+                return;
+            }
+
+            transitionEffect = (TransitionEffect) transitionEffect.Clone();
+
+            DoubleAnimation da = TransitionAnimation.Clone();
+            da.From = 0;
+            da.To = 1;
+            da.FillBehavior = FillBehavior.HoldEnd;
+
+            transitionEffect.OldImage = new VisualBrush(oldContentVisual);
+            transitionEffect.BeginAnimation(TransitionEffect.ProgressProperty, da);
+
+            _contentPresenter.Effect = transitionEffect;
+            _contentPresenter.Content = content;
         }
     }
 }

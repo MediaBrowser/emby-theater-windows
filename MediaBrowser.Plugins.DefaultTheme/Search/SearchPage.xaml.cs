7700004df86d8356f8d60b31e6ae07f02d9f3db7
shaun arundell <shaun.arundell@gmail.com>
2014-03-02 16:58:13 +1100

    Fixed search page navigation bug

--- MediaBrowser.Plugins.DefaultTheme/Search/SearchPage.xaml.cs
+++ MediaBrowser.Plugins.DefaultTheme/Search/SearchPage.xaml.cs
@@ -1,30 +1,17 @@
-﻿using System.Diagnostics;
-using System.Windows.Forms;
-using System.Windows.Navigation;
+﻿using System.Windows.Navigation;
 using MediaBrowser.Model.ApiClient;
 using MediaBrowser.Model.Dto;
-using MediaBrowser.Model.Entities;
 using MediaBrowser.Model.Logging;
-using MediaBrowser.Model.Querying;
 using MediaBrowser.Theater.Interfaces.Navigation;
 using MediaBrowser.Theater.Interfaces.Playback;
 using MediaBrowser.Theater.Interfaces.Presentation;
 using MediaBrowser.Theater.Interfaces.Session;
-using MediaBrowser.Theater.Interfaces.UserInput;
 using MediaBrowser.Theater.Presentation.Controls;
-using MediaBrowser.Theater.Presentation.Extensions;
 using MediaBrowser.Theater.Presentation.Pages;
-using MediaBrowser.Theater.Presentation.ViewModels;
-using MediaBrowser.Plugins.DefaultTheme.ListPage;
 using System;
 using System.ComponentModel;
 using System.Windows.Controls;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Input;
-using KeyEventArgs = System.Windows.Forms.KeyEventArgs;
 using ListBox = System.Windows.Controls.ListBox;
 
 namespace MediaBrowser.Plugins.DefaultTheme.Search
@@ -32,46 +19,47 @@ namespace MediaBrowser.Plugins.DefaultTheme.Search
     /// <summary>
     /// Interaction logic for FolderPage.xaml
     /// </summary>
-    public partial class SearchPage : BasePage, ISupportsItemThemeMedia, IItemPage
+    public partial class SearchPage : BasePage, ISearchPage, ISupportsItemThemeMedia, IItemPage
     {
         private readonly IApiClient _apiClient;
         private readonly IImageManager _imageManager;
+        private readonly INavigationService _navigationService;
         private readonly ILogger _logger;
         
         private readonly BaseItemDto _parentItem;
         private readonly SearchViewModel _searchViewModel;
 
-        public SearchPage(BaseItemDto parent, IApiClient apiClient, ISessionManager sessionManager, IImageManager imageManager, IPresentationManager presentation, INavigationService navigationManager, IPlaybackManager playbackManager, ILogger logger, IServerEvents serverEvents)
+        public SearchPage(BaseItemDto parent, IApiClient apiClient, ISessionManager sessionManager, IImageManager imageManager, IPresentationManager presentation, INavigationService navigatioService, IPlaybackManager playbackManager, ILogger logger, IServerEvents serverEvents)
         {
             _logger = logger;
             _imageManager = imageManager;
+            _navigationService = navigatioService;
             _apiClient = apiClient;
             _parentItem = parent;
            
             InitializeComponent();
 
             this.Loaded += SearchPage_Loaded;
-            
-            _searchViewModel = new SearchViewModel(presentation, _imageManager, _apiClient, sessionManager, navigationManager, playbackManager, _logger, serverEvents);
+
+            _searchViewModel = new SearchViewModel(presentation, _imageManager, _apiClient, sessionManager, navigatioService, playbackManager, _logger, serverEvents);
 
             _searchViewModel.MatchedItemsViewModel.PropertyChanged += PropertyChanged;
             _searchViewModel.MatchedPeopleViewModel.PropertyChanged += PropertyChanged;
-            //_searchViewModel.PropertyChanged += PropertyChanged;
-
+         
             DataContext = _searchViewModel;
         }
 
-        private void NavigationServiceOnNavigating(object sender, NavigatingCancelEventArgs navigatingCancelEventArgs)
+        private void _nav_Navigating(object sender, NavigatingCancelEventArgs navigatingCancelEventArgs)
         {
-            if (navigatingCancelEventArgs.NavigationMode == NavigationMode.Back)
+            var win = this.GetWindow();
+            if (win != null)    // win will only be !null when we are navigating away from search page
             {
-                // must be return to previous page, unhook the search - can't use unloaded event, window is already gone
-                var win = this.GetWindow();
-                if (win != null)
-                    win.PreviewKeyDown -= SearchPage_OnPreviewKeyDown;
+                // unhook the keydown here, we don't have a window to unhook from on the unLoaded event
+                win.PreviewKeyDown -= SearchPage_OnPreviewKeyDown;
+                this.NavigationService.Navigating -= _nav_Navigating; // this method
             }
         }
-      
+        
         void SearchPage_Loaded(object sender, RoutedEventArgs e)
         {
             SetPageTitle("Search");
@@ -79,11 +67,15 @@ namespace MediaBrowser.Plugins.DefaultTheme.Search
             var win = this.GetWindow();
             win.PreviewKeyDown += SearchPage_OnPreviewKeyDown;
 
-            this.NavigationService.Navigating += NavigationServiceOnNavigating;
+            //use the WPF NavigationService.Navigating event rather that the navigationManager.navigated event
+            // as we need tp get the Window of the page before we navigate away from it 
+            this.NavigationService.Navigating += _nav_Navigating;
         }
+
 
         private void SearchPage_OnPreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
+            _logger.Debug("SearchPage_OnPreviewKeyDown {0}", e.Key);
             ExtendedListBox x = AlphabetMenu;
             _searchViewModel.AlphaInputViewModel.UserInput_PreviewKeyDown(sender, e, AlphabetMenu);
         }
@@ -141,5 +133,9 @@ namespace MediaBrowser.Plugins.DefaultTheme.Search
             get { return _parentItem; }
         }
 
+        public void Dispose()
+        {
+            _logger.Debug("SearchPage dispose");
+        }
     }
 }

4ead4283263a48557782e6e2dcf21994aa6cff36
Thomas Gillen <thomas.gillen@googlemail.com>
2014-04-03 19:35:24 +0100

    Similar items section for item details page

--- MediaBrowser.Theater/MediaBrowser.Theater.DefaultTheme/ItemDetails/Views/ItemOverviewView.xaml.cs
+++ MediaBrowser.Theater/MediaBrowser.Theater.DefaultTheme/ItemDetails/Views/ItemOverviewView.xaml.cs
@@ -1,28 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace MediaBrowser.Theater.DefaultTheme.ItemDetails.Views
 {
     /// <summary>
-    /// Interaction logic for ItemOverviewView.xaml
+    ///     Interaction logic for ItemOverviewView.xaml
     /// </summary>
     public partial class ItemOverviewView : UserControl
     {
         public ItemOverviewView()
         {
             InitializeComponent();
+
+            GotKeyboardFocus += (s, e) => BringIntoView();
         }
     }
 }

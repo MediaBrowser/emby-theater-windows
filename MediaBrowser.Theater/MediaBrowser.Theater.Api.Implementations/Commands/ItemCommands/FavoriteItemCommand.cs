410bdd28a02ff20bc0df8b128952798ffd5087ca
Thomas Gillen <thomas.gillen@googlemail.com>
2015-04-19 14:23:41 +0100

    Added toggle watched and favorite item commands

---
+++ MediaBrowser.Theater/MediaBrowser.Theater.Api.Implementations/Commands/ItemCommands/FavoriteItemCommand.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using MediaBrowser.Model.ApiClient;
+using MediaBrowser.Model.Dto;
+using MediaBrowser.Theater.Api.Session;
+using MediaBrowser.Theater.Api.UserInterface;
+using MediaBrowser.Theater.Presentation.ViewModels;
+
+namespace MediaBrowser.Theater.Api.Commands.ItemCommands
+{
+    public class FavoriteItemCommand : BaseViewModel
+    {
+        private readonly IConnectionManager _connectionManager;
+        private readonly ISessionManager _sessionManager;
+
+        public FavoriteItemCommand(IConnectionManager connectionManager, ISessionManager sessionManager)
+        {
+            _connectionManager = connectionManager;
+            _sessionManager = sessionManager;
+        }
+
+        public bool IsEnabled { get; private set; }
+        public string DisplayName { get; private set; }
+        public ICommand ExecuteCommand { get; private set; }
+        public IViewModel IconViewModel { get; private set; }
+
+        public int SortOrder
+        {
+            get { return 70; }
+        }
+
+        public async Task Initialize(BaseItemDto item)
+        {
+            IsEnabled = true;
+            DisplayName = "Favorite";
+            IconViewModel = new DislikeItemCommandViewModel(item);
+            ExecuteCommand = new RelayCommand(o => {
+                IApiClient api = _connectionManager.GetApiClient(item);
+
+                bool isFavorite = item.UserData.IsFavorite;
+                api.UpdateFavoriteStatusAsync(item.Id, _sessionManager.CurrentUser.Id, !isFavorite);
+                item.UserData.IsFavorite = !isFavorite;
+            });
+        }
+    }
+}
---
+++ MediaBrowser.Theater/MediaBrowser.Theater.Api.Implementations/Commands/ItemCommands/WatchedItemCommandViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel;
+using MediaBrowser.Model.Dto;
+using MediaBrowser.Theater.Api.UserInterface;
+
+namespace MediaBrowser.Theater.Api.Commands.ItemCommands
+{
+    public class WatchedItemCommandViewModel : BaseViewModel
+    {
+        private readonly PropertyChangedEventHandler _isPlayedChanged;
+        private readonly BaseItemDto _item;
+        private readonly PropertyChangedEventHandler _userDataChanged;
+        private UserItemDataDto _data;
+        private bool _isPlayed;
+
+        public WatchedItemCommandViewModel(BaseItemDto item)
+        {
+            _item = item;
+            _data = item.UserData;
+
+            _isPlayedChanged = (s, e) => {
+                if (e.PropertyName == "Played") {
+                    UpdateIsPlayed(item);
+                }
+            };
+
+            _userDataChanged = (s, e) => {
+                if (e.PropertyName == "UserData") {
+                    _data.PropertyChanged -= _isPlayedChanged;
+                    _data = item.UserData;
+                    _data.PropertyChanged += _isPlayedChanged;
+
+                    UpdateIsPlayed(item);
+                }
+            };
+
+            _data.PropertyChanged += _isPlayedChanged;
+            _item.PropertyChanged += _userDataChanged;
+
+            UpdateIsPlayed(item);
+        }
+
+        public bool IsPlayed
+        {
+            get { return _isPlayed; }
+            private set
+            {
+                if (Equals(_isPlayed, value)) {
+                    return;
+                }
+
+                _isPlayed = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected override void OnClosed()
+        {
+            _item.PropertyChanged -= _userDataChanged;
+            _data.PropertyChanged -= _isPlayedChanged;
+
+            base.OnClosed();
+        }
+
+        private void UpdateIsPlayed(BaseItemDto item)
+        {
+            IsPlayed = item.UserData.Played;
+        }
+    }
+}

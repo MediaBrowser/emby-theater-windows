dad02a58a51a4553944d417366f6a0ddc8c70c05
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-05-26 22:31:19 -0400

    update updater

--- MediaBrowser.UI/ApplicationUpdater.cs
+++ MediaBrowser.UI/ApplicationUpdater.cs
@@ -19,29 +19,33 @@ namespace MediaBrowser.UI
                 version = File.ReadAllText(archive + ".ver");
             }
 
+            var systemPath = appPaths.ProgramSystemPath;
+
             // Use our installer passing it the specific archive
             // We need to copy to a temp directory and execute it there
-            var source = Path.Combine(appPaths.ProgramSystemPath, UpdaterExe);
+            var source = Path.Combine(systemPath, UpdaterExe);
 
             logger.Info("Copying updater to temporary location");
             var tempUpdater = Path.Combine(Path.GetTempPath(), UpdaterExe);
             File.Copy(source, tempUpdater, true);
-            source = Path.Combine(appPaths.ProgramSystemPath, UpdaterDll);
+            source = Path.Combine(systemPath, UpdaterDll);
             var tempUpdaterDll = Path.Combine(Path.GetTempPath(), UpdaterDll);
 
             logger.Info("Copying updater dependencies to temporary location");
             File.Copy(source, tempUpdaterDll, true);
             var product = "mbt";
             // Our updater needs SS and ionic
-            source = Path.Combine(appPaths.ProgramSystemPath, "ServiceStack.Text.dll");
+            source = Path.Combine(systemPath, "ServiceStack.Text.dll");
             File.Copy(source, Path.Combine(Path.GetTempPath(), "ServiceStack.Text.dll"), true);
-            source = Path.Combine(appPaths.ProgramSystemPath, "SharpCompress.dll");
+            source = Path.Combine(systemPath, "SharpCompress.dll");
             File.Copy(source, Path.Combine(Path.GetTempPath(), "SharpCompress.dll"), true);
 
             logger.Info("Starting updater process.");
 
-            var args = string.Format("product={0} archive=\"{1}\" caller={2} pismo=false version={3} service={4} installpath=\"{5}\"",
-                    product, archive, Process.GetCurrentProcess().Id, version, string.Empty, appPaths.ProgramDataPath);
+            // installpath = program data folder
+            // start path = executable to launch
+            var args = string.Format("product={0} archive=\"{1}\" caller={2} pismo=false version={3} service={4} installpath=\"{5}\" startpath=\"{6}\" systempath=\"{7}\"",
+                    product, archive, Process.GetCurrentProcess().Id, version, string.Empty, appPaths.ProgramDataPath, appPaths.ApplicationPath, systemPath);
 
             logger.Info("Args: {0}", args);
             Process.Start(tempUpdater, args);

b5f41f3c8478671168c55900a48792f0ef105213
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-04-23 16:55:01 -0400

    removed dead code

--- Emby.Theater/electronapp/scripts/windowsync.js
+++ Emby.Theater/electronapp/scripts/windowsync.js
@@ -1,51 +1,6 @@
 ﻿require(['apphost', 'events'], function (apphost, events) {
     'use strict';
 
-    var windowDragRegion;
-
-    function sendWindowStateCommand(e) {
-
-        var state = e.detail.windowState;
-
-        if (!windowDragRegion) {
-            windowDragRegion = document.querySelector('.windowDragRegion');
-        }
-
-        if (state.toLowerCase() == 'normal') {
-            windowDragRegion.classList.remove('nodrag');
-        } else {
-            windowDragRegion.classList.add('nodrag');
-        }
-
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'http://localhost:8154/windowstate-' + state, true);
-
-        xhr.send();
-    }
-
-    function sendWindowSizeCommand() {
-
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'http://localhost:8154/windowsize', true);
-        xhr.send();
-    }
-
-    document.addEventListener('windowstatechanged', sendWindowStateCommand);
-    window.addEventListener('resize', sendWindowSizeCommand);
-    window.addEventListener('move', sendWindowSizeCommand);
-
-    sendWindowSizeCommand({ detail: { windowState: apphost.getWindowState() } });
-
-    function sendCommand(name) {
-
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'electronapphost://' + name, true);
-
-        xhr.send();
-    }
-
-    sendCommand('loaded');
-
     function sendRunAtStartupConfigValue(appSettings) {
         var value = appSettings.get('runatstartup');
         var xhr = new XMLHttpRequest();

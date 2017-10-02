7a5fdb4568ea4db56141d2b6629b6b1038cced06
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-10-01 23:02:37 -0400

    2.9.2

--- Emby.Theater/App/ApplicationPaths.cs
+++ Emby.Theater/App/ApplicationPaths.cs
@@ -1,11 +1,12 @@
-﻿using Emby.Theater.AppBase;
+﻿using System.IO;
+using Emby.Theater.AppBase;
 
 namespace Emby.Theater.App
 {
     public class ApplicationPaths : BaseApplicationPaths
     {
         public ApplicationPaths(string programDataPath, string applicationPath)
-            : base(programDataPath, applicationPath)
+            : base(programDataPath, Path.GetDirectoryName(applicationPath))
         {
         }
     }
